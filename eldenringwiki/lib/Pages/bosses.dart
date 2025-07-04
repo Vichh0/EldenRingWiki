b43078bb39a769932038daae6b2087892b357476
Vicente Castillo M <vicentecastillomedina44@gmail.com>
2025-07-04 12:57:07 -0400

    ehhhh
    no se

---
+++ eldenringwiki/lib/Pages/bosses.dart
@@ -0,0 +1,19 @@
+class Bosses {
+  
+  String ID;
+  String Nombre;
+  String region;
+  String descripcion;
+  String locacion;
+  String drops;
+
+  Bosses({
+    required this.ID,
+    required this.Nombre,
+    required this.region,
+    required this.descripcion,
+    required this.locacion,
+    required this.drops,
+  });
+
+}
